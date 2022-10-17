91b009acd004f1f0c4e74afbc03d111c1c11f603
Vladimir <veliseykin2000@gmail.com>
2022-10-17 14:48:06 -0700

    Ino

--- bullet.py
+++ bullet.py
@@ -6,7 +6,7 @@ class Bullet(pygame.sprite.Sprite):
         # Пуля в позиции пушки
         super(Bullet, self).__init__()
         self.screen = screen
-        self.rect = pygame.Rect(0, 0, 2, 12)
+        self.rect = pygame.Rect(0, 0, 6, 12)
         self.color = 139, 195, 74
         self.speed = 1.5
         self.rect.centerx = gun.rect.centerx
--- space_game.py
+++ space_game.py
@@ -3,6 +3,7 @@ from gun import Gun
 from pygame.sprite import Group
 
 
+
 def run():
     pygame.init()
     screen = pygame.display.set_mode((700, 800))
@@ -10,12 +11,15 @@ def run():
     bg_color = (0, 0, 0)
     gun = Gun(screen)
     bullets = Group()
+    inos = Group()
+    controls.create_army(screen, inos)
 
     while True:
         controls.events(screen, gun, bullets)  
         gun.update_gun()
-        controls.update(bg_color, screen, gun, bullets)
+        controls.update(bg_color, screen, gun, inos, bullets)
         controls.update_bullets(bullets)
+        controls.update_inos(inos)
 
 
 run()
